490c613a1826a054294ed37b5bb5071b41f25f94
yonatan afewerk <yonatanafewerk@gmail.com>
2023-09-09 17:23:54 +0300

    making a blog ining site

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-2xl font-extrabold w-fit p-4  ">SillyBlogs</h1>
+        </>
+    );
+}
+
+export default App;
