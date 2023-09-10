4add871204c718c265cbc892fbb6d292c7e60411
yonatan afewerk <yonatanafewerk@gmail.com>
2023-09-10 19:46:40 +0300

    data added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import "./index.css";
 
 function App() {
-    return (
-        <>
-            <h1 className="text-2xl font-extrabold w-fit p-4  ">SillyBlogs</h1>
-        </>
-    );
+    return <></>;
 }
 
 export default App;
